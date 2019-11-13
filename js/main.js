0ee012b0a6049d3adf656c8419916b61c5153e73
alex <barmaleyalex@gmail.com>
2019-11-13 11:46:55 -0800

    Added lazy loading fixed  navbar bug

--- js/main.js
+++ js/main.js
@@ -11,3 +11,36 @@ function stickFooter() {
         $("#main-footer").removeClass("fixed-bottom");
     }
 }
+document.addEventListener("DOMContentLoaded", () => {
+    const options = {
+      root:null,// its the viewport
+      threshold: 0, //if 1 then 100% of item needs to be on the page
+      rootMargin: "-50px" 
+  };
+      var lazyImages =[].slice.call(
+       document.querySelectorAll("img.lazy")
+      )
+      
+      if ("IntersectionObserver" in window) {
+         let lazyImageObserver = 
+           new IntersectionObserver((entries, observer) => {
+             entries.forEach(function(entry) {
+               if (entry.isIntersecting) {
+                 let lazyImage = entry.target;
+                 lazyImage.src = lazyImage.dataset.src;
+                 lazyImage.srcset = lazyImage.dataset.srcset;
+                 lazyImage.classList.remove("lazy");
+                 lazyImage.classList.add("appear");
+                 lazyImageObserver.unobserve(lazyImage);
+                 console.log(lazyImages);
+               }
+             });
+           },options);
+     
+         lazyImages.forEach(function(lazyImage) {
+           lazyImageObserver.observe(lazyImage);
+         });
+       } else {
+         // Possibly fall back to a more compatible method here
+       }
+     });
